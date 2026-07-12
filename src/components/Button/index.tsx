8565848a99e6a36d3b7128e2662217a27066fe8b
lysialeao <lysialeao@gmail.com>
2026-07-12 10:05:29 -0300

    fix: :lipstick: change style to export

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,21 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 export function Button({ children, ...props }: ButtonProps) {
   return (
     <button
-      className="rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+      className="
+              inline-flex items-center justify-center
+              px-6 py-2
+              rounded-md
+              text-white
+              bg-green-700
+              hover:bg-green-800
+              active:bg-green-900
+              transition
+              focus:outline-none
+              focus:ring-2
+              focus:ring-green-400
+              focus:ring-offset-2
+              font-medium
+            "
       {...props}
     >
       {children}
